cbcb218c83ffe1ce72173989692609ec08f24ecb
adrianapereira27 <pereira.adri@gmail.com>
2024-01-08 07:00:03 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,62 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            // buscar todos ou filtrar
+            return Ok();
+        }
+
+        // api/projects/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // buscar o projeto
+            // return NotFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+            // cadastrar o projeto            
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+            // atualizo o objeto
+
+            return NoContent();
+        }
+
+        // api/projects/3 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // buscar, se não existir, retorna NotFound
+            
+            // remover
+            return NoContent();
+        }
+
+    }
+}
